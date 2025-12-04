024af4743bfed09749df4e85176953c5a83d2bc2
Romain Delamare <romain@delama.re>
2025-12-04 21:15:49 +0100

    day 4, part 2

--- src/day4.rs
+++ src/day4.rs
@@ -1,23 +1,46 @@
+use std::cell::OnceCell;
 use std::str::FromStr;
 
 use crate::{Error, Result, Solution};
 
 const INPUT: &str = include_str!("../input/day4");
 
-pub struct Day4;
+#[derive(Default)]
+pub struct Day4(OnceCell<Warehouse<136>>);
+
+impl Day4 {
+	fn warehouse(&self) -> Result<&Warehouse<136>> {
+		self.0.get_or_try_init(|| INPUT.parse())
+	}
+}
 
 impl Solution for Day4 {
 	fn part_one(&self) -> Result<String> {
-		let warehouse: Warehouse<136> = INPUT.parse()?;
-		let nb_accessible_rolls = warehouse.count_accessible_rolls();
+		let nb_accessible_rolls = self.warehouse()?.count_accessible_rolls();
 		Ok(format!("Number of accessible rolls: {nb_accessible_rolls}"))
 	}
 
 	fn part_two(&self) -> Result<String> {
-		todo!()
+		let (_, removed) = remove_all_rolls(self.warehouse()?);
+		Ok(format!("Number of removed rolls: {removed}"))
 	}
 }
 
+fn remove_all_rolls<const N: usize>(warehouse: &Warehouse<N>) -> (Warehouse<N>, usize) {
+	let mut current = *warehouse;
+	let mut total_removed = 0;
+	loop {
+		let (new_warehouse, removed) = current.remove_rolls();
+		if removed == 0 {
+			break;
+		}
+		current = new_warehouse;
+		total_removed += removed;
+	}
+	(current, total_removed)
+}
+
+#[derive(Debug, Eq, PartialEq, Copy, Clone)]
 struct Warehouse<const N: usize>([[bool; N]; N]);
 
 impl<const N: usize> Warehouse<N> {
@@ -66,6 +89,23 @@ impl<const N: usize> Warehouse<N> {
 			}
 		}
 		accessible_rows
+	}
+
+	fn remove_rolls(&self) -> (Self, usize) {
+		let mut spaces = [[false; N]; N];
+		let mut removed = 0;
+		for (r, row) in self.0.iter().enumerate() {
+			for (c, has_roll) in row.iter().enumerate() {
+				if *has_roll {
+					if self.count_adjacent_rolls(c, r) >= 4 {
+						spaces[r][c] = true;
+					} else {
+						removed += 1;
+					}
+				}
+			}
+		}
+		(Self(spaces), removed)
 	}
 }
 
@@ -143,5 +183,53 @@ mod test {
 	fn count_accessible_rolls_should_return_13_for_example() {
 		let warehouse: Warehouse<10> = EXAMPLE.parse().unwrap();
 		assert_eq!(warehouse.count_accessible_rolls(), 13);
+	}
+
+	#[test]
+	fn remove_rolls_example() {
+		let warehouse: Warehouse<10> = EXAMPLE.parse().unwrap();
+		let (new_warehouse, removed) = warehouse.remove_rolls();
+		assert_eq!(removed, 13);
+		assert_eq!(
+			new_warehouse,
+			"\
+.......@..
+.@@.@.@.@@
+@@@@@...@@
+@.@@@@..@.
+.@.@@@@.@.
+.@@@@@@@.@
+.@.@.@.@@@
+..@@@.@@@@
+.@@@@@@@@.
+....@@@...
+"
+			.parse::<Warehouse<10>>()
+			.unwrap()
+		);
+	}
+
+	#[test]
+	fn remove_all_rolls_example() {
+		let warehouse: Warehouse<10> = EXAMPLE.parse().unwrap();
+		let (new_warehouse, removed) = remove_all_rolls(&warehouse);
+		assert_eq!(removed, 43);
+		assert_eq!(
+			new_warehouse,
+			"\
+..........
+..........
+..........
+...x@@....
+...@@@@...
+...@@@@@..
+...@.@.@@.
+...@@.@@@.
+...@@@@@..
+....@@@...
+"
+			.parse::<Warehouse<10>>()
+			.unwrap()
+		);
 	}
 }
